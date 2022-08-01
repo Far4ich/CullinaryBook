a5430d22e3b840332f2c07b7469708515a0350a1
Far4ich <79937711+Far4ich@users.noreply.github.com>
2022-08-01 16:06:57 +0300

    Add home page

--- CullinaryBookSite/ClientApp/src/app/app.module.ts
+++ CullinaryBookSite/ClientApp/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { MainPageComponent } from './components/pages/main/main-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CullinaryBookSite/ClientApp/src/app/components/pages/main/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl:'./main-page.component.html',
+    styleUrls: ['main-page.css']
+})
+
+export class MainPageComponent{
+    
+}
