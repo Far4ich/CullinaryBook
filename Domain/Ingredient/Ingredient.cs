1f6a314e66ae8d1bc921c91675e9ee3b12873e5f
Far4ich <79937711+Far4ich@users.noreply.github.com>
2022-08-12 13:08:59 +0300

    Add order step and ingredient

--- Domain/Ingredient/Ingredient.cs
+++ Domain/Ingredient/Ingredient.cs
@@ -2,16 +2,18 @@
 {
     public class Ingredient
     {
-        public Ingredient(int id, string title, string products, int recipeId)
+        public Ingredient(int id, string title, int order, string products, int recipeId)
         {
             Id = id;
             Title = title;
+	    Order = order;
             Products = products;
             RecipeId = recipeId;
         }
 
         public int Id { get; private set; }
         public string Title { get; private set; }
+        public int Order { get; private set; }
         public string Products { get; private set; }
         public int RecipeId { get; private set; }
     }
