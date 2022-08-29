b9b1c51c56e2e5b535758dccf6e1b64b6a2f5695
Far4ich <79937711+Far4ich@users.noreply.github.com>
2022-08-29 13:39:52 +0300

    Remove EF from Api

--- Infrastructure/Services/ServiceCollectionsExtensions.cs
+++ Infrastructure/Services/ServiceCollectionsExtensions.cs
@@ -9,16 +9,7 @@ namespace Infrastructure.Services
         public static void AddEntityFramework(this IServiceCollection serviceCollection, string connectionString)
         {
             serviceCollection.AddDbContext<CullinaryBookContext>(option =>
-            {
-                try
-                {
-                    option.UseSqlServer(connectionString, b => b.MigrationsAssembly("Infrastructure.Migrations"));
-                }
-                catch (Exception)
-                {
-
-                }
-            });
+                    option.UseSqlServer(connectionString));       
         }
     }
 }
